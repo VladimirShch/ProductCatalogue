4931933cab31d81fee730441c94ff953b6773c79
VladimirShch <43771483+VladimirShch@users.noreply.github.com>
2023-03-20 19:18:34 +0300

    Dialogs styled

---
+++ Presentation/Dialogs/Views/MessageWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ProductCatalogue.WPF.Presentation.Dialogs.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для MessageWindow.xaml
+    /// </summary>
+    public partial class MessageWindow : Window
+    {
+        public MessageWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
